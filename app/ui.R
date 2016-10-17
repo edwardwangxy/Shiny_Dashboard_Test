942f78aedbf1a689a631f37a3337d441705b3d39
Edward Wang <edwardwangxy@gmail.com>
2016-10-16 22:06:25 -0400

    add an insertion sort function in function_test add tutorial shinydashboard functions into this dashboard

--- app/ui.R
+++ app/ui.R
@@ -15,8 +15,13 @@ shinyUI(
         selectInput('xvar', 'Pick Length', names(airquality)[c(5,6)],
                     selected=names(airquality)[[6]]),
         # actionButton('select2', 'Select the above variables.'),
-        sliderInput("singlemonth", label = "View each month",
-                    min = 5, max = 9, value = 5, step = 1),
+        selectInput('xcol', 'X Variable', names(airquality)[-c(5,6)],
+                    selected=names(airquality)[[1]]),
+        selectInput('ycol', 'Y Variable', names(airquality)[-c(5,6)],
+                    selected=names(airquality)[[2]]),
+        # actionButton('select2', 'Select the above variables.'),
+        sliderInput("subsample", label = "Size of random samples",
+                    min = 5, max = 50, value = 10, step = 1),
         actionButton('resetSelection',
                      label = "Click to reset row selection"
         ) # end of action button
@@ -27,7 +32,9 @@ shinyUI(
       # Boxes need to be put in a row (or column)
       fluidRow(
         box(plotOutput("plot1", height = 300)),
-        box(plotOutput("plot2", height = 300)),
+        box(column(6, h1('select rows'),DT::dataTableOutput('x1',width = 450))),
+        box(column(6, plotOutput('x2', width = 450))),
+        box(verbatimTextOutput('info')),
         verbatimTextOutput("action")
       )
     )
---
+++ function_test.R
@@ -0,0 +1,30 @@
+insertion.sort <- function(A){
+  for(i in c(2:length(A))){
+    for(j in c(i:2)){
+      if(A[j-1]>A[j]){
+        temp_val <- A[j-1]
+        A[j] <- A[j-1]
+        A[j-1]=temp_val
+      }
+    }
+  }
+  return(A)
+}
+
+result <- insertion.sort(tosort)
+tosort <- c(4,2,5,1,3)
+length(tosort)
+insertion.sort(tosort)
+tosort[0]
+tosort
+rosort[1]
+tosort[1]
+for(i in c(2:length(tosort))){
+  for(j in c(i:2)){
+    if(tosort[j-1]>tosort[j]){
+      temp_val <- tosort[j]
+      tosort[j] <- tosort[j-1]
+      tosort[j-1]=temp_val
+    }
+  }
+}
