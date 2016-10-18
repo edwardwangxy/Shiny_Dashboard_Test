026d00f7a9ad11848fdbfdd562bbfe14e4407f78
Edward Wang <edwardwangxy@gmail.com>
2016-10-18 16:53:02 -0400

    add insertion function using while loop

--- insertion_sort_function.R
+++ insertion_sort_function.R
@@ -12,7 +12,21 @@ insertion.sort <- function(A){
 }
 
 
+insertion.sort2 <- function(A){
+  for(i in c(2:length(A))){
+    j = i
+    while(j>1 && A[j-1]>A[j]){
+        temp_val <- A[j-1]
+        A[j-1] <- A[j]
+        A[j]=temp_val
+        j = j-1
+    }
+  }
+  return(A)
+}
+
 tosort <- c(4,2,5,1,3)
 result <- insertion.sort(tosort)
 result
 insertion.sort(c(1,5,2,7,3,6))
+insertion.sort2(c(1,5,2,7,3,6))
